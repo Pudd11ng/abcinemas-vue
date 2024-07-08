17d62adcb1c594bf055e84376b92a58e350e7b97
MJ <chaijing13691@gmail.com>
2024-07-09 00:18:06 +0800

    profile

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import VueTelInput from 'vue-tel-input';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@/assets/css/style-starter.css';
+import 'vue-tel-input/vue-tel-input.css';
+
 // import '@/assets/css/progress.css';
 // import '@/assets/css/ticket-booking.css';
 // import '@/assets/css/e-ticket.css';
@@ -17,4 +20,5 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(VueTelInput);
 app.mount('#app');

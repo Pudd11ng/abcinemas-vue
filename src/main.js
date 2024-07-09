30201f7c6f3ce17473617cce46b4ddcbfeacc6d9
MJ <chaijing13691@gmail.com>
2024-07-10 00:26:20 +0800

    admin user management

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import VueTelInput from 'vue-tel-input';
+import CanvasJSChart from '@canvasjs/vue-charts';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap'
 import '@/assets/css/style-starter.css';
 import 'vue-tel-input/vue-tel-input.css';
 
@@ -21,4 +22,5 @@ const pinia = createPinia();
 app.use(pinia);
 app.use(router);
 app.use(VueTelInput);
+app.use(CanvasJSChart);
 app.mount('#app');

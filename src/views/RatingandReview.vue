41df7b115eca50c07c6f7f7d60f827b78dfcbfcd
MJ <chaijing13691@gmail.com>
2024-07-14 11:29:41 +0800

    juner review

--- src/views/RatingandReview.vue
+++ src/views/RatingandReview.vue
@@ -16,20 +16,32 @@
           <h3>{{ selectedMovie.title }}</h3>
           <div class="reviews">
             <h4>Ratings & Reviews</h4>
-            <ul>
-              <li v-for="review in selectedMovie.reviews" :key="review.review_id">
+            <ul
+              v-if="selectedMovie.reviews && selectedMovie.reviews.length > 0"
+            >
+              <li
+                v-for="review in selectedMovie.reviews"
+                :key="review.review_id"
+              >
                 <em>{{ review.rating }}/5</em> - {{ review.review }}
                 <button @click="editReview(review)">Edit</button>
                 <button @click="deleteReview(review.review_id)">Delete</button>
               </li>
             </ul>
-            <button @click="toggleReviewForm">{{ isEditing ? 'Cancel Edit' : 'Add Review' }}</button>
+            <p v-else>No reviews available for this movie.</p>
+            <button @click="toggleReviewForm">
+              {{ isEditing ? "Cancel Edit" : "Add Review" }}
+            </button>
             <div v-if="showReviewForm" class="add-review">
-              <h4>{{ isEditing ? 'Update' : 'Add' }} a Review</h4>
+              <h4>{{ isEditing ? "Update" : "Add" }} a Review</h4>
               <form @submit.prevent="submitReview">
                 <div>
                   <label for="rating">Rating:</label>
-                  <select id="rating" v-model.number="newReview.rating" required>
+                  <select
+                    id="rating"
+                    v-model.number="newReview.rating"
+                    required
+                  >
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -39,12 +51,21 @@
                 </div>
                 <div>
                   <label for="review">Review:</label>
-                  <textarea id="review" v-model="newReview.review" required></textarea>
+                  <textarea
+                    id="review"
+                    v-model="newReview.review"
+                    required
+                  ></textarea>
                 </div>
-                <button type="submit">{{ isEditing ? 'Update Review' : 'Submit Review' }}</button>
+                <button type="submit">
+                  {{ isEditing ? "Update Review" : "Submit Review" }}
+                </button>
               </form>
             </div>
           </div>
+        </div>
+        <div v-else>
+          <p>Loading...</p>
         </div>
       </div>
     </main>
@@ -52,10 +73,10 @@
 </template>
 
 <script>
-import HeaderPage from '@/components/HeaderPage.vue';
+import HeaderPage from "@/components/HeaderPage.vue";
 
 export default {
-  name: 'RatingReviewsPage',
+  name: "RatingReviewsPage",
   components: {
     HeaderPage,
   },
@@ -66,65 +87,81 @@ export default {
       selectedMovie: null,
       newReview: {
         rating: null,
-        review: ''
+        review: "",
       },
       showReviewForm: false,
       isEditing: false,
-      editReviewId: null
+      editReviewId: null,
     };
   },
   methods: {
     fetchMovies() {
-      fetch('movies.php')
-        .then(response => response.json())
-        .then(data => {
+      fetch("http://localhost:8088/movies")
+        .then((response) => response.json())
+        .then((data) => {
           this.movies = data;
-          this.selectedMovieId = this.movies.length > 0 ? this.movies[0].id : null;
+          console.log(this.movies);
+          this.selectedMovieId =
+            this.movies.length > 0 ? this.movies[0].id : null;
           this.updateSelectedMovie();
+        })
+        .catch((error) => {
+          console.error("Error fetching movies:", error);
         });
     },
     updateSelectedMovie() {
       if (this.selectedMovieId) {
-        fetch(`rating_reviews.php?movie_id=${this.selectedMovieId}`)
-          .then(response => response.json())
-          .then(data => {
-            this.selectedMovie = this.movies.find(movie => movie.id === this.selectedMovieId);
+        fetch(`http://localhost:8088/movies/${this.selectedMovieId}`)
+          .then((response) => response.json())
+          .then((data) => {
+            this.selectedMovie = this.movies.find(
+              (movie) => movie.id === this.selectedMovieId
+            );
             if (this.selectedMovie) {
               this.selectedMovie.reviews = data;
             }
+          })
+          .catch((error) => {
+            console.error("Error fetching selected movie:", error);
           });
       }
     },
     submitReview() {
       const formData = new FormData();
-      formData.append('movieID', this.selectedMovieId);
-      formData.append('rating', this.newReview.rating);
-      formData.append('review', this.newReview.review);
+      formData.append("movieID", this.selectedMovieId);
+      formData.append("rating", this.newReview.rating);
+      formData.append("review", this.newReview.review);
 
       if (this.isEditing) {
-        formData.append('review_id', this.editReviewId);
+        formData.append("review_id", this.editReviewId);
 
-        fetch('rating_reviews.php', {
-          method: 'PUT',
-          body: formData
+        fetch("http://localhost:8088/reviews", {
+          method: "PUT",
+          body: formData,
         })
-        .then(response => response.text())
-        .then(data => {
-          console.log(data);
-          this.updateSelectedMovie();  // Refresh the reviews list
-          this.resetForm();
-        });
+          .then((response) => response.text())
+          .then((data) => {
+            console.log(data);
+            this.updateSelectedMovie(); // Refresh the reviews list
+            this.resetForm();
+          })
+          .catch((error) => {
+            console.error("Error updating review:", error);
+          });
       } else {
-        fetch('rating_reviews.php', {
-          method: 'POST',
-          body: formData
+        fetch("http://localhost:8088/reviews", {
+          method: "POST",
+          body: formData,
         })
-        .then(response => response.text())
-        .then(data => {
-          console.log(data);
-          this.updateSelectedMovie();  // Refresh the reviews list
-          this.resetForm();
-        });
+          .then((response) => response.text())
+          .then((data) => {
+            console.log(data);
+            this.updateSelectedMovie(); // Refresh the reviews list
+            this.resetForm();
+          })
+          .catch((error) => {
+            console.error("Error submitting review:", error);
+          });
       }
     },
     editReview(review) {
@@ -135,14 +172,17 @@ export default {
       this.showReviewForm = true;
     },
     deleteReview(reviewId) {
-      fetch(`rating_reviews.php?review_id=${reviewId}`, {
-        method: 'DELETE'
+      fetch(`http://localhost:8088/reviews/${reviewId}`, {
+        method: "DELETE",
       })
-      .then(response => response.text())
-      .then(data => {
-        console.log(data);
-        this.updateSelectedMovie();  // Refresh the reviews list
-      });
+        .then((response) => response.text())
+        .then((data) => {
+          console.log(data);
+          this.updateSelectedMovie(); // Refresh the reviews list
+        })
+        .catch((error) => {
+          console.error("Error deleting review:", error);
+        });
     },
     toggleReviewForm() {
       this.showReviewForm = !this.showReviewForm;
@@ -151,14 +191,14 @@ export default {
       }
     },
     resetForm() {
-      this.newReview = { rating: null, review: '' };
+      this.newReview = { rating: null, review: "" };
       this.isEditing = false;
       this.editReviewId = null;
-    }
+    },
   },
   mounted() {
     this.fetchMovies();
-  }
+  },
 };
 </script>
 
